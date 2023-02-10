fd5a7c6b4a087c5ce289ce3c954e47f451c11b66
Rajeshwar Voleti <118740244+rajeshvoleti17@users.noreply.github.com>
2023-02-10 21:37:49 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
-//call by reference
 #include<stdio.h>
-void Swap(int *a,int *b);
+#include<stdlib.h>
 int main()
 {
-    int a=10,b=25;
-    printf("before swapping,value a=%d b=%d\n",a,b);
-    Swap(&a,&b);
-    printf("after swapping,value a=%d b=%d\n",a,b);
+    int i,n,*ptr;
+    printf("enter the number of integers :");
+    scanf("%d",&n);
+    ptr=(int*)malloc(2*sizeof(int));
+    if(ptr==NULL)
+    {
+        printf("Insufficient memory");
+        exit(1);
+    }
+    printf("\n Memory allocated successfully and base address = %u\n",ptr);
+    printf("Default value stored is : \n");
+    for(i=0;i<2;i++)
+    {
+        printf("%d stored at %u address \n",*(ptr+i),(ptr+i));
+    }
+    printf("enter 2 integer numbers :");
+    for(i=0;i<n;i++)
+        scanf("%d",(ptr+i));
+    printf("enter integer numbers are :\n");
+    for(i=0;i<2;i++)
+        printf("%d stored at %u address \n",*(ptr+i),(ptr+i));
+    ptr=(int*)realloc(ptr,4*sizeof(int));
+    if(ptr==NULL)
+    {
+        printf("insufficient memory");
+        exit(1);
+    }
+    printf("enter two more integer numbers :\n");
+    for(i=2;i<4;i++)
+        scanf("%d",(ptr+i));
+    
+    printf("entered integer numbers are :\n");
+    for(i=0;i<4;i++)
+        printf("%d stored at %u address \n",*(ptr+i),(ptr+i));
+    free (ptr);
+    return 0;
 }
-void Swap(int *a,int *b)
-{
-    int temp;
-    temp=*a;
-    *a=*b;
-    *b=temp;
-    printf("inside function,valueof a=%d b=%d",*a,*b);
-}
-
-
